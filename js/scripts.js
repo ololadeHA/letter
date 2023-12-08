d8d2faa37dbf10f9080dbe8bb4f1be38dc8d0de8
Arowolo Haleem <arowolohaleem@gmail.com>
2023-12-08 11:45:54 +0100

    add form code to html page

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(e) {
+    const name1Input = $("input#name1").val();
+    const name2Input = $("input#name2").val();
+    const addressInput= $("input#address").val();
+    const stateInput = $("input#state").val();
+    const email1Input = $("input#email1").val();
+
+    $(".name1").text(name1Input);
+    $(".name2").text(name2Input);
+    $(".address").text(addressInput);
+    $(".state").text(stateInput);
+    $(".email1").text(email1Input);
+
+  $(".letter").show();
+
+  e.preventDefault()
+ });
+
+})
